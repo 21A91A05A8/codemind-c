89e32a02e410d9e615aedb4fad8e4fa0e5f1c865
21A91A05A8 <97424469+21A91A05A8@users.noreply.github.com>
2022-02-23 19:14:01 +0530

    #include <stdio.h> int gcd(int x, int y) { 	return y == 0 ? x : gcd(y, x % y); }
    int lcm(int x, int y)
    {
    	return x * y / gcd(x, y);
    }
    
    int main()
    {
    	int a, b;
    	scanf("%d %d", &a, &b);
    	printf("%d",lcm(a, b));
    }

---
+++ LCM.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int gcd(int x, int y)
+{
+	return y == 0 ? x : gcd(y, x % y);
+}
+
+int lcm(int x, int y)
+{
+	return x * y / gcd(x, y);
+}
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d",lcm(a, b));
+}
