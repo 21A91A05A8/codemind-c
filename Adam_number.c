617185bc350cef71a012ad3231f9d81d60cb354f
21A91A05A8 <97424469+21A91A05A8@users.noreply.github.com>
2022-04-18 20:46:14 +0530

    #include<stdio.h> int main() {     int n,k,d,e,rev=0,i,jrev=0,j;     scanf("%d",&n);     k=n;     i=k*k;     while(n!=0)     {         d=n%10;         rev=rev*10+d;         n=n/10;     }     j=rev*rev;     while(j!=0)     {         e=j%10;         jrev=jrev*10+e;         j=j/10;     }     if(jrev==i)     {         printf("True");     }     else{         printf("False");     } }

--- Adam_number.c
+++ Adam_number.c
@@ -1,29 +1,28 @@
 #include<stdio.h>
 int main()
 {
-    int n,k,d,e,rev=0,jrev=0,i,j;
+    int n,k,d,e,rev=0,i,jrev=0,j;
     scanf("%d",&n);
     k=n;
     i=k*k;
     while(n!=0)
     {
         d=n%10;
-        rev=(rev*10)+d;
+        rev=rev*10+d;
         n=n/10;
     }
     j=rev*rev;
     while(j!=0)
     {
         e=j%10;
-        jrev=(jrev*10)+e;
+        jrev=jrev*10+e;
         j=j/10;
     }
     if(jrev==i)
     {
         printf("True");
     }
-    else
-    {
+    else{
         printf("False");
     }
 }
