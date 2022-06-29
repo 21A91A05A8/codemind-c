2d3374d21c85c81dfe6eaaed954df792886bcf2c
21A91A05A8 <97424469+21A91A05A8@users.noreply.github.com>
2022-06-29 20:49:00 +0530

    #include<stdio.h> #include<stdlib.h> int main() {     int arr[100],i,n,sum1=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(i%2)         {             sum1+=arr[i];         }     }     printf("%d",sum1); }

--- Sum_of_Odd_Positions.c
+++ Sum_of_Odd_Positions.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-    int arr[100],n,i,sum=0;
+    int arr[100],i,n,sum1=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
@@ -11,8 +12,8 @@ int main()
     {
         if(i%2)
         {
-            sum+=arr[i];
+            sum1+=arr[i];
         }
     }
-    printf("%d",sum);
+    printf("%d",sum1);
 }
